34acefe764ecf0a6cf1e7e180cd30a34ba8a5855
alidiba67 <ali.diba67@gmail.com>
2015-12-07 12:17:06 +0100

    first commit

--- net_stat.m
+++ net_stat.m
@@ -5,7 +5,7 @@ nchannel = zeros(1, length(net.layers));
 for i=1:length(net.layers)
     t = net.layers{i}.type;
     if strcmp(t, 'conv') == 1        
-        nchannel(i) = size(net.layers{i}.filters, 4);
+        nchannel(i) = size(net.layers{i}.weights{1,1}, 4);
     else
         nchannel(i) = nchannel(i-1);
     end
